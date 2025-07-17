8017a499569deb5f07c78219c402817c88b203bd
Oladipupo Ishola <olaishola@hotmail.co.uk>
2025-07-17 13:52:34 +0100

    parametrized & patch for memoize

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,7 +4,7 @@
 
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 from unittest.mock import patch, MagicMock
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -43,6 +43,30 @@ class TestGetJson(unittest.TestCase):
     def test_get_json(self, test_url, test_payload, mock_requests):
         mock_requests.json.return_value = test_payload
         self.assertTrue(get_json(test_url), test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """
+    A test class to test the memoize decorator
+    """
+
+    def test_memoize(self):
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, "a_method", return_value=42) as mock_method:
+            obj = TestClass()
+            result1 = obj.a_property
+            result2 = obj.a_property
+
+            self.assertEqual(result1, 42)
+            self.assertEqual(result2, 42)
+            mock_method.assert_called_once()
 
 if __name__ == '__main__':
     unittest.main(argv=['first-arg-is-ignored'], exit=False)
