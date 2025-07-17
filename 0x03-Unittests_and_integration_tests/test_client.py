61648ac1b5d4ad28972f1f887174d59d14b42b4e
Oladipupo Ishola <olaishola@hotmail.co.uk>
2025-07-17 19:56:08 +0100

    debugging test 2

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -11,25 +11,20 @@ class TestGithubOrgClient(unittest.TestCase):
     """Test class for GithubOrgClient.org"""
 
     @parameterized.expand([
-        ("google", {"repos_url": "https://api.github.com/orgs/google/repos"}),
-        ("abc", {"repos_url": "https://api.github.com/orgs/abc/repos"}),
+        ("google",),
+        ("abc",),
     ])
-    @patch('utils.get_json')
-    def test_org(self, org_name, expected_data, mock_get_json):
-        """Test that GithubOrgClient.org returns
-        correct data and get_json is called once
-        """
-        # url = f"https://api.github.com/orgs/{org_name}/repos"
-        # expected_payload = {"repos_url": url}
-        mock_get_json.return_value = expected_data
+    @patch("utils.get_json")
+    def test_org(self, org_name, mock_get_json):
+        url = f"https://api.github.com/orgs/{org_name}/repos"
+        expected_payload = {"repos_url": url}
+        mock_get_json.return_value = expected_payload
 
         client = GithubOrgClient(org_name)
         result = client.org
 
-        expected_url = client.ORG_URL.format(org=org_name)
-        # print(f"Expected result: {result}")
-        # print(f"Expected data: {expected_data}")
-        self.assertEqual(result, expected_data)
+        expected_url = GithubOrgClient.ORG_URL.format(org=org_name)
+        self.assertEqual(result, expected_payload)
         mock_get_json.assert_called_once_with(expected_url)
 
 
