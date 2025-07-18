250f177ac06ab5338698fa9e64ed3190c23c6018
Oladipupo Ishola <olaishola@hotmail.co.uk>
2025-07-18 10:53:42 +0100

    reorder test case

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -26,10 +26,11 @@ class TestGithubOrgClient(unittest.TestCase):
 
         client = GithubOrgClient(org_name)
         result = client.org
-        self.assertEqual(result, expected_payload)
 
         expected_url = f"https://api.github.com/orgs/{org_name}"
         mock_get_json.assert_called_once_with(expected_url)
+
+        self.assertEqual(result, expected_payload)
 
 
 if __name__ == '__main__':
