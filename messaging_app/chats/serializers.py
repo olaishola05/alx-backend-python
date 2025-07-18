57e2ab411b983f51e6c93e635e795968a9cf612f
Oladipupo Ishola <olaishola@hotmail.co.uk>
2025-07-18 23:11:43 +0100

    fix submission errors

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -1,28 +1,38 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from models import Message, Conversation, User
+from .models import Message, Conversation
 
-# User = get_user_model()
+User = get_user_model()
 
 
 class UserSerializer(serializers.ModelSerializer):
+    phone_number = serializers.CharField(max_length=15)
+    profile_photo = serializers.URLField(max_length=200)
+    email = serializers.EmailField(unique=True)
+    is_online = serializers.BooleanField(default=False)
+
     class Meta:
         model = User
         fields = ['user_id', 'first_name', 'last_name', 'email']
 
 
 class MessageSerializer(serializers.ModelSerializer):
-    sender_id = serializers.UUIDField(source='sender.user_id', read_only=True)
+    sender_name = serializers.SerializerMethodField()
+
+    def get_sender_name(self, obj):
+        return f"{obj.sender.first_name} {obj.sender.last_name}"
 
     class Meta:
         model = Message
-        fields = ['message_id', 'sender_id', 'message_body', 'sent_at']
+        fields = ['message_id', 'sender_id', 'message_body', 'sent_at', 'sender_name']
 
 
 class ConversationSerializer(serializers.ModelSerializer):
-    messages = MessageSerializer(many=True, read_only=True)
-    participants = serializers.SlugRelatedField(slug_field='user_id', many=True, read_only=True)
+    def validate(self, data):
+        if data['participants'].count() < 2:
+            raise serializers.ValidationError("A conversation must include at least 2 participants.")
+        return data
 
     class Meta:
         model = Conversation
-        fields = ['conversation_id', 'participants', 'created_at', 'messages']
+        fields = '__all__'
