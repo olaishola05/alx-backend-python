b162345491e43a455ce93b2b7df69a093967d96e
Oladipupo Ishola <olaishola@hotmail.co.uk>
2025-07-18 18:47:08 +0100

    update chat models to match requirements

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid
 
-# Create your models here.
+
+class User(AbstractUser):
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    profile_picture = models.URLField(max_length=200, blank=False, null=True)
+    is_online = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    def __str__(self):
+        return self.username
+
+
+class Conversation(models.Model):
+    conversation_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    participants = models.ManyToManyField(User)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Conversation {self.conversation_id}"
+
+
+class Message(models.Model):
+    message_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, to_field='user_id')
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, to_field='conversation_id')
+    message_body = models.TextField()
+    sent_at = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_created=True)
+
+    def __str__(self):
+        return f"From {self.sender.username} at {self.sent_at}"
