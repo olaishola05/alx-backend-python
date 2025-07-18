e32ea1fc87ff07e25202b52703da951b9b1264bd
Oladipupo Ishola <olaishola@hotmail.co.uk>
2025-07-18 23:45:16 +0100

    Create viewsets for converstion & message

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
+from rest_framework import viewsets, permissions
 
-# Create your views here.
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint to get all conversations
+    """
+    queryset = Conversation.objects.all().order_by('created_at')
+    serializer_class = ConversationSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        # Only show conversations the current user is a part of
+        return self.queryset.filter(participants=self.request.user)
+
+    def perform_create(self, serializer):
+        conversation = serializer.save()
+        conversation.participants.add(self.request.user)
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint to get all the Messages
+    """
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return self.queryset.filter(conversation__participants=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(sender=self.request.user)
