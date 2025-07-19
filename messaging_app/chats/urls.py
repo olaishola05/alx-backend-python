fdfb5d9c2ef5a9adb279facf88ab6a081682928b
Oladipupo Ishola <olaishola@hotmail.co.uk>
2025-07-19 14:07:01 +0100

    fix router import and instantiation

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
 from .views import (ConversationViewSet, MessageViewSet)
-from rest_framework.routers import DefaultRouter
+from rest_framework import routers
 
-router = DefaultRouter()
+router = routers.DefaultRouter()
 
 router.register(r'conversations', ConversationViewSet, basename='conversation')
 router.register(r'messages', MessageViewSet, basename='message')
